6559134bd55c0f99be860934add65d05d007fcac
VinayakGawade009 <vinayakgawade009@gmail.com>
2026-02-02 15:03:21 +0530

    fetching notes (queries)

--- convex/schema.ts
+++ convex/schema.ts
@@ -1,9 +1,15 @@
-import { defineSchema } from "convex/server";
+import { defineSchema, defineTable } from "convex/server";
 import { authTables } from "@convex-dev/auth/server";
+import { v } from "convex/values";
 
 const schema = defineSchema({
-    ...authTables
-    // Your other tables
+    ...authTables,
+    
+    notes: defineTable({
+        title: v.string(),
+        body: v.string(),
+        userId: v.id("users"),
+    }).index("by_userId", ["userId"]),
 });
 
 export default schema;
