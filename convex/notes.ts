0e3914dfc9479e752f760758afd4ff4e79751170
VinayakGawade009 <vinayakgawade009@gmail.com>
2026-02-02 15:35:37 +0530

    Deleting notes mutation

--- convex/notes.ts
+++ convex/notes.ts
@@ -35,6 +35,30 @@ export const createNote = mutation({
             title: args.title,
             body: args.body,
             userId,
-        })
-    }
-})
+        });
+    },
+});
+
+export const deleteNote = mutation({
+    args: {
+        noteId: v.id("notes"),
+    },
+    handler: async (ctx, args) => {
+        const userId = await getAuthUserId(ctx);
+        if(!userId) {
+            throw new Error("User must be authenticated to delete a note");
+        }
+
+        const note = await ctx.db.get(args.noteId);
+
+        if (!note) {
+            throw new Error ("Note not found");
+        }
+
+        if (note.userId !== userId) {
+            throw new Error("User is not authorized to delete this note");
+        }
+
+        await ctx.db.delete(args.noteId);
+    },
+});
--- src/app/(main)/notes/note-item.tsx
+++ src/app/(main)/notes/note-item.tsx
@@ -2,24 +2,33 @@
 
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Doc } from "../../../../convex/_generated/dataModel";
+import { NotePreviewDialog } from "./note-preview-dialog";
 
 interface NoteItemProps {
   note: Doc<"notes">
 }
 
 export function NoteItem({note}: NoteItemProps) {
+  function handleOpenNote() {
+    window.history.pushState(null, "", `?noteId=${note._id}`) // this is faster than router.push (little optimization)
+  }
+
   return (
     <>
-      <Card className="cursor-pointer hover:shadow-md transition-shadow">
+      <Card 
+        className="cursor-pointer hover:shadow-md transition-shadow"
+        onClick={handleOpenNote}
+      >
         <CardHeader>
           <CardTitle>{note.title}</CardTitle>
         </CardHeader>
         <CardContent>
           <div className="line-clamp-3 text-sm text-muted-foreground whitespace-pre-line">
-            {note.body}
+            {note.body }
           </div>
         </CardContent>
       </Card>
+      <NotePreviewDialog note={note} />
     </>
   );
 }
