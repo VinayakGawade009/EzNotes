1098d3ca91de19e35c0e83154ee7d20caea4d221
VinayakGawade009 <vinayakgawade009@gmail.com>
2026-02-03 11:43:50 +0530

    Basic chatbot (streamText, useChat, http actions, keyboard inputs, autoscroll, loading animation, initial ai message, Error Message, message triming)

--- src/app/(main)/notes/ai-chat-button.tsx
+++ src/app/(main)/notes/ai-chat-button.tsx
@@ -1,14 +1,25 @@
 "use client";
 
+import Markdown from "@/components/markdown";
 import { Button } from "@/components/ui/button";
 import { Textarea } from "@/components/ui/textarea";
 import { cn } from "@/lib/utils";
+import { useAuthToken } from "@convex-dev/auth/react";
 import { Bot, Expand, Minimize, Send, Trash, X } from "lucide-react";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import { useChat } from "@ai-sdk/react";
+import { DefaultChatTransport, UIMessage } from "ai";
+
+const convexSiteUrl =
+  process.env.NEXT_PUBLIC_CONVEX_URL?.replace(
+    /.cloud$/,
+    ".site"
+  )
 
 export function AIChatButton() {
   const [chatOpen, setChatOpen] = useState(false);
 
+  console.log(convexSiteUrl);
   return (
     <>
       <Button onClick={() => setChatOpen(true)} variant="outline">
@@ -25,12 +36,63 @@ interface AIChatBoxProps {
   onClose: () => void;
 }
 
+const initialMessages: UIMessage[] = [
+  {
+    id: "welcome-message",
+    role: "assistant",
+    parts: [
+      {
+        type: "text",
+        text: "I'm your notes assistant. I can find and summarize any information that you saved."
+      }
+    ]
+  }
+]
+
 function AIChatBox({ open, onClose }: AIChatBoxProps) {
+  const [input, setInput] = useState("");
+
   const [isExpanded, setIsExpanded] = useState(false);
+
+  const token = useAuthToken();
+
+  const { messages, sendMessage, setMessages, status } = useChat({
+    transport: new DefaultChatTransport({
+      api: `${convexSiteUrl}/api/chat`,
+      headers: {
+        Authorization: `Bearer ${token}`
+      },
+    }),
+    messages: initialMessages,
+  });
 
   const messagesEndRef = useRef<HTMLDivElement>(null);
 
+  const isProcessing = status === "submitted" || status === "streaming";
+
+  useEffect(() => {
+    if (open) {
+      messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+    }
+  }, [open, messages]); // we want to scroll down only when open changes or messages arrat changes
+
+  function onSubmit(e: React.FormEvent) {
+    e.preventDefault();
+    if (input.trim() && !isProcessing) {
+      sendMessage({ text: input });
+      setInput("");
+    }
+  }
+
+  const handleKeyDown = (e: React.KeyboardEvent) => {
+    if (e.key === "Enter" && !e.shiftKey) { // Enter -> sends message, shift + Enter -> new line
+      onSubmit(e);
+    }
+  };
+
   if (!open) return null;
+
+  const lastMessageIsUser = messages[messages.length - 1].role === "user";
 
   return (
     <div
@@ -59,11 +121,12 @@ function AIChatBox({ open, onClose }: AIChatBoxProps) {
           <Button
             variant="ghost"
             size="icon"
-            onClick={() => {}}
+            onClick={() => setMessages(initialMessages)} // after deleting previous chat history we still want initial welcome message in aiChatBox
             className="text-primary-foreground hover:bg-primary/90 h-8 w-8"
             title="Clear chat"
+            disabled={isProcessing}
           >
-            <Trash />
+            <Trash />  {/* deletes messages from aiChatBox */}
           </Button>
           <Button
             variant="ghost"
@@ -77,21 +140,71 @@ function AIChatBox({ open, onClose }: AIChatBoxProps) {
       </div>
 
       <div className="flex-1 space-y-4 overflow-y-auto p-3">
-        {/* TODO: Render messages here */}
+        {messages.map((message) => (
+          <ChatMessage key={message.id} message={message} />
+        ))}
+        { status === "submitted" && lastMessageIsUser && <Loader /> } {/* loading indicator or ai message */}
+        { status === "error" && <ErrorMessage />}
         <div ref={messagesEndRef} />
       </div>
 
-      <form className="flex gap-2 border-t p-3">
+      <form className="flex gap-2 border-t p-3" onSubmit={onSubmit}>
         <Textarea
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          onKeyDown={handleKeyDown}
           placeholder="Type your message..."
           className="max-h-[120px] min-h-[40px] resize-none overflow-y-auto"
           maxLength={1000}
           autoFocus
         />
-        <Button type="submit" size="icon">
+        <Button 
+          type="submit" 
+          size="icon"
+          disabled={!input.trim() || isProcessing} // disables when no message to send or ai is processing
+        >
           <Send className="size-4" />
         </Button>
       </form>
+    </div>
+  );
+}
+
+interface ChatMessageProps {
+  message: UIMessage
+}
+
+function ChatMessage({ message }: ChatMessageProps) {
+  const currentStep = message.parts[message.parts.length - 1];
+
+  return (
+    <div
+      className={cn(
+        "mb-2 flex max-w-[80%] flex-col prose dark:prose-invert",
+        message.role === "user" ? "ml-auto items-end" : "mr-auto items-start"
+      )}
+    >
+      <div
+        className={cn(
+          "prose dark:prose-invert rounded-lg px-3 py-2 text-sm",
+          message.role === "user"
+            ? "bg-primary text-primary-foreground"
+            : "bg-muted first:prose-p:mt-0"
+        )}
+      >
+        {message.role === "assistant" && (
+          <div className="text-muted-foreground mb-1 flex items-center gap-1 text-xs font-medium">
+            <Bot className="text-primary size-3" />
+            AI Assistant
+          </div>
+        )}
+        {currentStep?.type === "text" && (
+          <Markdown>{currentStep.text}</Markdown>
+        )}
+        {currentStep.type === "tool-invocation" && (
+          <div className="italic animate-pulse">Searching notes...</div>
+        )}
+      </div>
     </div>
   );
 }
@@ -104,4 +217,10 @@ function Loader() {
       <div className="bg-primary size-1.5 animate-pulse rounded-full delay-300" />
     </div>
   );
+}
+
+function ErrorMessage() {
+  return <div className="text-sm text-red-500">
+    Something went wrong. Please try again.
+  </div>
 }
